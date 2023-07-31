202a7b4af2f043d7b32b181e82013f73e6b5b17a
Klaidonas <klaidas.vysn@gmail.com>
2023-07-31 18:43:15 +0300

    vis meta errora, kad neegzistuoja vertimas

---
+++ components/ExampleClientComponent.js
@@ -0,0 +1,5 @@
+'use client';
+
+export default function ExampleClientComponent() {
+  return <h2>Hello from a Client Component!</h2>;
+}
---
+++ middleware.js
@@ -0,0 +1,14 @@
+import { i18nRouter } from 'next-i18n-router';
+import i18nConfig from './i18nConfig';
+
+export function middleware(request) {
+  return i18nRouter(request, {
+    locales: ["en","de","ja"],
+    defaultLocale: "en"
+  });
+}
+
+// applies this middleware only to files in the app directory
+export const config = {
+  matcher: '/((?!api|static|.*\\..*|_next).*)'
+};
